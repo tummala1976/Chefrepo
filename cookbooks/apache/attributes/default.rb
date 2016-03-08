c45ca84687ac6a628410b6d1866bf1eecf9199d7
root <root@convergysrft1.mylabserver.com>
2016-03-08 22:57:46 +0000

    Chef repo for forst time

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,2 @@
+default["apache"]["sites"]["convergysrft2"] = { "site_title" => "convergysrft2 website coming soon", "port" => 80, "domain"=> "convergysrft2.mylabserver.com" }
+default["apache"]["sites"]["convergysrft2b"] = { "site_title" => "convergysrft2b website coming soon", "port" => 80, "domain"=> "convergysrft2b.mylabserver.com" }
